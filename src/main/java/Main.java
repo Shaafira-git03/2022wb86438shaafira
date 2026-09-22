323df33da246a112d8fd5c88ec79357d82adbf2f
Shaafira-git03 <2022wb86438@wilp.bits-pilani.ac.in>
2026-09-22 20:54:45 +0530

    Update greeting message in Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from 2022WB86438");
+        System.out.println("Hello from continuous integration is working");
     }
 }
